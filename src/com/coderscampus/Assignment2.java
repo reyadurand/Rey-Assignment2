63b80b77f091e249b843e4f7a57ef1c919d36009
ReyR <duran.rey.2024@gmail.com>
2023-12-11 12:43:46 -0500

    Update Assignment 2

--- src/com/coderscampus/Assignment2.java
+++ src/com/coderscampus/Assignment2.java
@@ -1,10 +1,43 @@
 package com.coderscampus;
 
+import java.util.Scanner;
+
 public class Assignment2 {
 
 	public static void main(String[] args) {
-		
+		int randomNumber = (int) (Math.random() * 100) + 1;
+	        int chances = 5;
+	        Scanner scanner = new Scanner(System.in);
 
+
+	        while (chances > 0) {
+	            System.out.print("Please pick a number between 1 and 100.");
+	            int guess = scanner.nextInt();
+
+	            if (guess < 1 || guess > 100) {
+	                System.out.println("Your guess is not between 1 and 100, please try again.");
+	                continue;
+	            }
+
+	            if (guess == randomNumber) {
+	                System.out.println("You win!");
+	                break;
+	            } else if (guess < randomNumber) {
+	                System.out.println("Please pick a higher number.");
+	            } else {
+	                System.out.println("Please pick a lower number.");
+	            } 
+	            
+	            
+	            chances--;
+	            System.out.println("Chances remaining: " + chances);
+	        }
+
+	        if (chances == 0) {
+	        	System.out.println("You lose!");
+	            System.out.println("The number to guess was: " + randomNumber);
+	        }
+
+	        scanner.close();
+	    }
 	}
-
-}
